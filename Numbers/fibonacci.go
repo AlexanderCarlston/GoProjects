56c736e774ee5f98de67c56ebf2dc89f91be2618
alekscarlston@yahoo.com <alekscarlston@yahoo.com>
2018-08-07 16:42:26 -0600

    added instructions to problems

--- Numbers/fibonacci.go
+++ Numbers/fibonacci.go
@@ -1,1 +1,3 @@
+// Enter a number and have the program generate the Fibonacci sequence to that number or to the Nth number.
+
 package main
--- Numbers/pi.go
+++ Numbers/pi.go
@@ -1,4 +1,4 @@
-// Instructions: find pi to the Nth digit.
+//  Enter a number and have the program generate PI up to that many decimal places. Keep a limit to how far the program will go.
 
 package main
 
