7623f7d0101790ea444b7644de7901631c046c99
alekscarlston@yahoo.com <alekscarlston@yahoo.com>
2018-08-06 22:42:08 -0600

    need to find the math

--- Numbers/pi.go
+++ Numbers/pi.go
@@ -1,4 +1,4 @@
-// Instructions: find pie to the Nth digit.
+// Instructions: find pi to the Nth digit.
 
 package main
 
@@ -8,5 +8,33 @@ import (
 )
 
 func main(){
+	var n int
+	// n is the NTH digit the user inputs
+	fmt.Printf("Compute π to how many decimal places? (≤200) ")
+	_, err := fmt.Scanf("%d", &n)
+	//Error handling
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	if n > 200 {
+		fmt.Println("Error:", n, "> 200 - too many decimal places!")
+		return
+	} else if n < 0 {
+		fmt.Println("Error:", n, "< 0")
+		return
+	}
+
 	
+
+}
+
+func factorialHelper(a int) *big.Int {
+	b := big.NewInt(1)
+
+	for i := a; i > 1; i-- {
+		b = b.Mul(b, big.NewInt(int64(i)))
+	}
+
+	return b
 }
