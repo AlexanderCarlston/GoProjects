cab0a2ceca3a34868d09b9a2e2a6ebf10758ae13
alekscarlston@yahoo.com <alekscarlston@yahoo.com>
2018-08-08 16:26:07 -0600

    edge casing

--- Numbers/primefactorization.go
+++ Numbers/primefactorization.go
@@ -4,5 +4,18 @@ package main
 import "fmt"
 
 func main(){
+	var n int
+
+	fmt.Println("Compute prime factors of what number?")
+	_, err := fmt.Scanf("%d", &n)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	if n < 2 {
+		fmt.Println("Error:", n, "is too small! Try an integer n ≥ 2.")
+		return
+	}
+
 	
 }
